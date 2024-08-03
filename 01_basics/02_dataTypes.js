fcc6427bbd72acb78f01753b889d1facfe2d0225
Vaibhav Shukla <51282485+vbhvshukla@users.noreply.github.com>
2024-08-03 11:33:37 +0000

    changed

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,68 +1,29 @@
-let score = "hitesh"
+"use strict"; // treat all JS code as newer version
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// alert( 3 + 3) // we are using nodejs, not browser
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
 
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
 
-let isLoggedIn = "hitesh"
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false
-// "hitesh" => true
+// object
 
-let someNumber = 33
-
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-
-// *********************** Operations ***********************
-
-let value = 3
-let negValue = -value
-// console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
-
-let str1 = "hello"
-let str2 = " hitesh"
-
-let str3 = str1 + str2
-// console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
-// console.log( (3 + 4) * 5 % 3);
-
-// console.log(+true);
-// console.log(+"");
-
-let num1, num2, num3
-
-num1 = num2 = num3 = 2 + 2
-
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
-
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

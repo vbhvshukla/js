d904b46ca11fac7d6f21944c885093deb867f112
Vaibhav Shukla <51282485+vbhvshukla@users.noreply.github.com>
2024-08-03 06:18:08 +0000

    something

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1123;
+let accountEmail = "Something@something.com";
+var accountPassword = "abc@123"
+accountCity = "Jaipur";
+let something;
+
+//trying to change each one of them
+//accountId = 112222 not allowed
+accountEmail = "Somme@sommee.commchanged"
+accountPassword ="changed@123"
+accountCity="Bengaluru"
+
+//To print in a tabular form use console.table
+
+console.table([
+    accountEmail,accountId,accountPassword,accountCity.something
+])
